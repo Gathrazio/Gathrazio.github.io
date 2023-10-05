1c56c434909a51a9998b13289bcdd100a19bea09
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-10-04 18:59:34 -0600

    updated portfolio

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,10 @@
-import Topic from './Topic'
-import gradCap from '../assets/grad-cap.png'
-import {DiJavascript1, DiCss3Full, DiHtml5, DiReact, DiGit, DiGithubBadge, DiMongodb } from 'react-icons/di'
+import Topic from './Topic';
+import {DiJavascript1, DiCss3Full, DiHtml5, DiReact, DiGit, DiGithubBadge, DiMongodb, DiPython} from 'react-icons/di';
 import { IconContext } from "react-icons";
-import { FaGraduationCap} from "react-icons/fa";
-import { SiLatex } from "react-icons/si";
+import { FaGraduationCap } from "react-icons/fa";
+import { SiLatex, SiFlask } from "react-icons/si";
 import { FaNodeJs } from 'react-icons/fa'
+import { TbBrandDjango } from 'react-icons/tb'
 import ProjectBlurb from './ProjectBlurb'
 
 export default function Body ({sections}) {
@@ -13,7 +13,7 @@ export default function Body ({sections}) {
             <Topic>
                 <h1 className="topic-title" ref={sections.about}>Who Am I?</h1>
                 <p className="topic-paragraph">
-                    I am a Full Stack JS Web Developer with an emphasis on the backend and a rigorous background in pure mathematics. I aspire to change the world. Delve into my <a href="https://github.com/Gathrazio" target="_blank">Github profile</a>.
+                    I am a Full Stack Software Engineer with an emphasis on the MERN stack and python-based web applications. I have a rigorous background in pure mathematics and extensive math tutoring experience. I aspire to change the world. Please delve into my <a href="https://github.com/Gathrazio" target="_blank">Github profile</a>, my <a href="https://www.linkedin.com/in/noahnsjensen/" target="_blank">LinkedIn profile</a>, or contact me via email at <a href="mailto: nnsjdev@gmail.com">nnsjdev@gmail.com</a>. I am able, willing, and experienced in adapting the value I can bring to any situation in any organization -- even yours!
                 </p>
             </Topic>
             <hr />
@@ -32,6 +32,11 @@ export default function Body ({sections}) {
                                 <DiMongodb />
                                 <FaNodeJs /> 
                             </div>
+                            <div className="icon-block center">
+                                <DiPython />
+                                <TbBrandDjango/>
+                                <SiFlask /> 
+                            </div>
                             <div className="icon-block bottom">
                                 <SiLatex />
                                 <DiGit />
@@ -39,7 +44,7 @@ export default function Body ({sections}) {
                             </div>
                         </IconContext.Provider>
                         <p className="topic-paragraph">
-                            I am fluent with the most recent iterations of Javascript, HTML, and CSS. I have also spent considerable time improving my skills with React.js, working with various backend technologies (like MongoDB, Node.js, Express.js, JWT authentication), and effectively utilizing Git and Github to work on personal and team projects. I also have over four years of intense practice in the math typesetting software LaTeX, and am fluent in a large swathe of collegiate mathematics. Really everything from statistics, linear algebra, calculus, number theory, discrete mathematics, and topology.
+                            I am fluent with the most recent iterations of Javascript, HTML, and CSS. I have also spent considerable time improving my skills with React.js, working with various backend technologies (like MongoDB, Node.js, Express.js, JWT authentication, and Python frameworks like Django, Flask). Throughout, I have effectively utilized Git and Github to work on personal and team projects. I also have over four years of intense practice in the math typesetting software LaTeX, and am fluent in a large swathe of collegiate mathematics. Really everything from statistics, linear algebra, calculus, number theory, discrete mathematics, and topology. My work in school focused on braids and knots, essentially combinatorial topology, and I spent considerable time building educational materials for calculus, statistics, and discrete math courses.
                         </p>
                     </li>
                     <li>
@@ -49,7 +54,10 @@ export default function Body ({sections}) {
                         </div>
                         </IconContext.Provider>
                         <p className="topic-paragraph">
-                            I graduated from USU with a Bachelors of Science degree in Mathematics with the Summa Cum Laude disctinction in 2022. I have also completed the skills portion of V School's Full Stack JS Web Development program.
+                            I graduated from USU with a Bachelors of Science degree in Mathematics with the Summa Cum Laude disctinction in May of 2022 (<a href="https://drive.google.com/drive/folders/1NnAjKZPGiPNixASZUuMd4Qp4QUIwAW7m?usp=sharing" target="_blank">view some of my work as a student and educator</a>). The deductive reasoning skills, problem solving methods, quantitative writing skills, and perseverance I gained from this degree has been incredibly important for my personal growth. I want to someday do something fantastical -- to push humanity's knowledge forward in some capacity. In particular, I desire to break through and solve the problem of artificial general intelligence (<a href="https://en.wikipedia.org/wiki/Artificial_general_intelligence" target="_blank">AGI</a>). For that purpose as well my degree stands as support.
+                        </p>
+                        <p className="topic-paragraph">
+                            Additionally, I graduated from <a href="https://drive.google.com/file/d/1TkmbmHhJXIyvH8rRr2UeIe7KhD14d1nz/view" target="_blank">V School's Full MERN Stack JS Web Development program</a> in August of 2023 (<a href="https://app.digit.ink/en/view-credential/85a60e1e-d806-43f8-b925-e5bb14e0a601?di_ref=a2li" target="_blank">see proof here</a>). To deepen my knowledge of web development and pursue a passion for machine learning, I obtained a 12-month professional certification in backend development from IBM (<a href="https://www.coursera.org/account/accomplishments/specialization/certificate/TAAYZZJS2XGP" target="_blank">see proof here</a>) in September of 2023 and completed <a href="https://www.andrewng.org/" target="_blank">Andrew Ng</a>'s Machine Learning course specialization at the same time (<a href="https://www.coursera.org/account/accomplishments/specialization/certificate/KDZY5TYAGPPT">see proof here</a>).
                         </p>
                     </li>
                 </ul>
@@ -58,15 +66,97 @@ export default function Body ({sections}) {
             <Topic>
                 <h1 className="topic-title" ref={sections.projects}>Projects</h1>
                 <p className="topic-paragraph">
-                            The live projects may take approximately 60 seconds to spin up due to the free hosting service's limitations.
+                            The live projects may take approximately 60 seconds to spin up due to the free hosting service's limitations. I built these projects during my 1000+ hour V School education and during the IBM 12-month backend development professional certificate. All of these projects (sans the Djirection site) were brainstormed, designed, implemented, debugged, and deployed by me alone. I learned many invaluable lessons about React component tree structure, the benefits and demerits of using React context, the appropriate usage patterns of React State, and intelligent utilizations of useEffect and other React hooks having to do with interacting with data.
                 </p>
                 <div className="project-blurbs-wrapper">
-                    <ProjectBlurb extraClass="" intro="Live MERN stack, JWT token authenticated social media & messaging service application:" projectName="Circulate" codeRef="https://github.com/Gathrazio/circulate" live="https://circulate.onrender.com"/>
-                    <ProjectBlurb extraClass="" intro="Live MERN stack, JWT token authenticated shopping application:" projectName="The Icecream Zone" codeRef="https://github.com/Gathrazio/level-5-full-stack-app" live="https://icecream-zone.onrender.com" apiDocs="https://github.com/Gathrazio/icecream-shop/blob/master/shop-api-documentation.md"/>
-                    <ProjectBlurb extraClass="" intro="Live MERN stack, JWT token authenticated issue forum application:" projectName="Issue Forum" codeRef="https://github.com/Gathrazio/issue-forum" live="https://issue-forum.onrender.com"/>
-                    <ProjectBlurb extraClass="" intro="Live CSS Zen Garden mimic using native HTML, CSS, and Javascript:" projectName="My Zen Garden" codeRef="https://github.com/Gathrazio/vs-assignments/tree/master/level-two/css-zen-garden" live="https://my-css-zen-garden.onrender.com/"/>
-                    <ProjectBlurb extraClass="" intro="Pure Javascript RPG:" projectName="" codeRef="https://github.com/Gathrazio/vs-assignments/tree/master/level-one/colossal-adventure-rpg"/>
-                    <ProjectBlurb extraClass="last-project" intro="Caesar Cipher Encryptor/Decryptor Utility:" projectName="" codeRef="https://github.com/Gathrazio/vs-assignments/tree/master/level-three/caesar-cipher"/>
+                    <ProjectBlurb
+                        extraClass=""
+                        intro="Live MERN stack, JWT token authenticated social media & messaging service application:"
+                        projectName="Circulate"
+                        firstButtonLink="https://circulate.onrender.com"
+                        secondButtonLink="https://github.com/Gathrazio/circulate"
+                        secondButtonText="View Code"
+                        description="This MERN stack mobile-first web application is a social media messaging service which allows people to make friends and communicate with each other privately without the need for a phone number. In building this single-page application, I displayed a working knowledge of HTML, CSS, JavaScript, React.js, Node.js, Express.js, MongoDB, JWT user authentication, Axios, and RESTful backend API design, development, and implementation."
+                        bullet1="Constructed a significantly complex backend API following RESTful standards."
+                        bullet2="Incorporated several layers of confidential information security using JSON Web Tokens and both the Bcrypt and Cryptr libraries to effectively hash passwords and encrypt messages.
+                        Utilized both synchronous and asynchronous programming to perform the necessary data-transfer tasks."
+                        bullet3="Created an accessible, dynamic, and responsive front-end by eloquently managing React state and useEffect instances with repeated asynchronous API calls (a web socket workaround), among other advanced React techniques, and ensuring that all data being viewed on the screen is validated securely first through the database and is up to date."
+                    />
+                    <ProjectBlurb
+                        extraClass=""
+                        intro="Live MERN stack, JWT token authenticated shopping application:"
+                        projectName="The Icecream Zone"
+                        firstButtonLink="https://icecream-zone.onrender.com"
+                        secondButtonLink="https://github.com/Gathrazio/level-5-full-stack-app"
+                        secondButtonText="View Code"
+                        thirdButtonText="View API Docs"
+                        thirdButtonLink="https://github.com/Gathrazio/icecream-shop/blob/master/shop-api-documentation.md"
+                        description="This MERN stack mobile-first web application is an e-commerce shopping service for a fake icecream business which allows potential customers to view products, add desired products in whatever quantity they want to their cart, checkout their cart, and view their past orders. In building this single-page application, I again displayed a working knowledge of the same software technologies used in Circulate."
+                        bullet1="Constructed a RESTful backend and also generated extremely thorough documentation so the API can potentially be of use to others and easily improved upon."
+                        bullet2="Gained experience in generating test data in the form of food items and in decoding the standardized UTC date format into a particular user's local time."
+                        bullet3="Created an advanced rendering scheme from scratch to conditionally render components and organized React state to work well with it."
+                        bullet4="Used several media queries and intelligent HTML structuring to re-design the user interface so that the site looks beautiful both on desktop and mobile devices."
+                    />
+                    <ProjectBlurb
+                        extraClass=""
+                        intro="Live MERN stack, JWT token authenticated issue forum application:"
+                        projectName="Issue Forum"
+                        firstButtonLink="https://issue-forum.onrender.com"
+                        secondButtonText="View Code"
+                        secondButtonLink="https://github.com/Gathrazio/issue-forum"
+                        description="This MERN stack mobile-first web application is a forum where people can login, view public issues rated by popularity, comment on those issues, and post their own. In building this single-page application, I displayed a working knowledge of HTML, CSS, JavaScript, React.js, Node.js, Express.js, MongoDB, JWT user authentication, Axios, and RESTful backend API design and implementation."
+                        bullet1="Designed and implemented a universal issue rating system in tandem with MongoDB so that the most popular issues will be viewable by any user in the public issue page."
+                        bullet2="Successfully orchestrated integral data operations between several MongoDB collections."
+                        bullet3="Implemented user issue posts and comments on said issues."
+                    />
+                    
+                    <ProjectBlurb
+                        extraClass=""
+                        intro="Djirection Band Site using Django, Flask, and microservices hosted on RedHat OpenShift and IBM Cloud:"
+                        secondButtonText="View Code"
+                        secondButtonLink="https://github.com/Gathrazio/Djirection-Band-Site"
+                        description="This Django web application, as my capstone for the IBM Professional Certificate, is the main website for a fake band that allows users to log in, view pictures taken at concerts, and RSVP for concerts in the future. It includes the use of two Flask-based microservices which serve the relevant concert pictures and songs. I built and utilized unit tests to ensure proper function of the various services. This project was at one point hosted locally along with the two microservices being hosted by IBM Cloud Engine and RedHat OpenShift, but due to cost that is no longer the case. In building this application, I displayed a working knowledge of HTML, CSS, JavaScript, MongoDB, SQL, Python, Django, Flask, and RESTful API design."
+                        bullet1="Constructed a RESTful Python API using both Django and Flask, and implemented the usage of both MongoDB and SQL."
+                        bullet2="Deployed microservices to cloud providers and utilized containerization via Docker and Kubernetes, among other services."
+                    />
+                    <ProjectBlurb
+                        extraClass=""
+                        intro="Songs microservice using Flask:"
+                        secondButtonText="View Code"
+                        secondButtonLink="https://github.com/Gathrazio/Back-End-Development-Songs"
+                        description="This microservice was hosted on RedHat OpenShift and utilized Flask's simple server architecture to perform CRUD operations on song data hosted in a MongoDB instance, also running on OpenShift."
+                    />
+                    <ProjectBlurb
+                        extraClass=""
+                        intro="Pictures microservice using Flask:"
+                        secondButtonText="View Code"
+                        secondButtonLink="https://github.com/Gathrazio/Back-End-Development-Pictures"
+                        description="This microservice was hosted on IBM Cloud and also utilized Flask's simple server architecture to perform CRUD operations on picture data hosted locally inside the server."    
+                    />
+                    <ProjectBlurb
+                        extraClass=""
+                        intro="Live CSS Zen Garden mimic using native HTML, CSS, and Javascript:"
+                        projectName="My Zen Garden"
+                        secondButtonLink="https://github.com/Gathrazio/vs-assignments/tree/master/level-two/css-zen-garden"
+                        secondButtonText="View Code"
+                        thirdButtonText="View Original"
+                        thirdButtonLink="https://www.csszengarden.com/"
+                        firstButtonLink="https://my-css-zen-garden.onrender.com/"
+                        description="This is a fully fleshed out mobile-first static site made with just vanilla HTML, CSS, and Javascript. It attempts to mimic the original CSS Zen Garden. This site primarily showcases my mastery over CSS and my ability to mimic website designs from scratch."
+                    />
+                    <ProjectBlurb
+                        extraClass=""
+                        intro="Pure Javascript RPG in Node.js:" 
+                        secondButtonLink="https://github.com/Gathrazio/vs-assignments/tree/master/level-one/colossal-adventure-rpg"
+                        secondButtonText="View Code"
+                        description=""
+                    />
+                    <ProjectBlurb
+                        extraClass="last-project"
+                        intro="Caesar Cipher Encryptor/Decryptor Utility in Node.js:"
+                        codeRef="https://github.com/Gathrazio/vs-assignments/tree/master/level-three/caesar-cipher"
+                        
+                    />
                 </div>
             </Topic>
         </div>
--- src/components/ProjectBlurb.jsx
+++ src/components/ProjectBlurb.jsx
@@ -13,10 +13,19 @@ export default function ProjectBlurb (props) {
                 }
             </span>
             <div className="blurb-buttons-wrapper">
-                {props.projectName && <button onClick={() => window.open(`${props.live}`,'_blank')} className="project-button">{props.projectName}</button>}
-                <button className="project-button" onClick={() => window.open(`${props.codeRef}`,'_blank')}>View Code</button>
-                {props.apiDocs && <button className="project-button" onClick={() => window.open(`${props.apiDocs}`, '_blank')}>View API Docs</button>}
+                {props.projectName && <button onClick={() => window.open(`${props.firstButtonLink}`,'_blank')} className="project-button">{props.projectName}</button>}
+                <button className="project-button" onClick={() => window.open(`${props.secondButtonLink}`,'_blank')}>{props.secondButtonText}</button>
+                {props.thirdButtonText && <button className="project-button" onClick={() => window.open(`${props.thirdButtonLink}`, '_blank')}>{props.thirdButtonText}</button>}
             </div>
+            <div className="blurb-description">
+                {props.description}
+            </div>
+            <ul>
+                {props.bullet1 && <li className="description-bullet">{props.bullet1}</li>}
+                {props.bullet2 && <li className="description-bullet">{props.bullet2}</li>}
+                {props.bullet3 && <li className="description-bullet">{props.bullet3}</li>}
+                {props.bullet4 && <li className="description-bullet">{props.bullet4}</li>}
+            </ul>
         </div>
     )
 
