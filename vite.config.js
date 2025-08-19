103e91f6ea644863f42e11021558e005cd812099
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2025-08-19 15:12:49 -0600

    replaced entire project with working gh-pages-host-test vite project

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/Gathrazio.github.io",
   plugins: [react()],
+  base: "/gh-pages-host-test",
 })
